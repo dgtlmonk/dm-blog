3a888ebbd81d3d1ad54b17473e1679d43f611852
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-05-26 12:30:59 +0800

    initial setup

---
+++ public/app/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('dmBlog',['ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+ /* .state('directive-1',{
+      url:'/d1',
+      templateUrl:'partials/directive-1.html'
+  }) */
+
+});
+
