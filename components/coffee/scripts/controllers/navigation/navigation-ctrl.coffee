8c601fd49d49efd6ef39c621584d9c8a3823cf6a
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-07-01 06:00:02 +0800

    fixing nav

--- components/coffee/scripts/controllers/navigation/navigation-ctrl.coffee
+++ components/coffee/scripts/controllers/navigation/navigation-ctrl.coffee
@@ -2,9 +2,22 @@ define ['scripts/controllers/module-base'], (module) ->
     'use strict'
     console.log 'navigation controller :: init'
     module.controller 'NavigationCtrl', ['$scope','$location', ($scope, $location) ->
+        $scope.isSelected = (section) ->
+            $scope.selected is section
+           
+
+        $scope.setSelection = (section) ->
+            console.log 'selection set to ' + section
+            $scope.selected = section
+            return
+
+
+
+
         $scope.navClass = (page) ->
             console.log $location.path().substring(1)
             currentRoute = $location.path().substring(1) || ''
+            alert 'yo'
             if page is currentRoute then 'active-nav' else ''
         return
     ]
--- public/app/scripts/controllers/navigation/navigation-ctrl.js
+++ public/app/scripts/controllers/navigation/navigation-ctrl.js
@@ -3,10 +3,18 @@ define(['scripts/controllers/module-base'], function(module) {
   console.log('navigation controller :: init');
   return module.controller('NavigationCtrl', [
     '$scope', '$location', function($scope, $location) {
+      $scope.isSelected = function(section) {
+        return $scope.selected === section;
+      };
+      $scope.setSelection = function(section) {
+        console.log('selection set to ' + section);
+        $scope.selected = section;
+      };
       $scope.navClass = function(page) {
         var currentRoute;
         console.log($location.path().substring(1));
         currentRoute = $location.path().substring(1) || '';
+        alert('yo');
         if (page === currentRoute) {
           return 'active-nav';
         } else {
